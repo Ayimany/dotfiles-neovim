c88227dcfd5d4a6936ef10dd2952f40b96c5911a
Ayimany <mooncatayimany@proton.me>
2024-08-04 21:06:37 -0600

    More icons!

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,21 +4,21 @@ return {
 
     opts = {
         signs = {
-            add          = { text = '󰐕 ' },
-            change       = { text = '󰧟 ' },
-            delete       = { text = '󰍴 ' },
-            topdelete    = { text = '󰅖 ' },
-            changedelete = { text = '󰏰 ' },
-            untracked    = { text = '󰍉 ' },
+            add          = { text = '' },
+            change       = { text = '' },
+            delete       = { text = '' },
+            topdelete    = { text = '' },
+            changedelete = { text = '' },
+            untracked    = { text = '' },
         },
 
         signs_staged = {
-            add          = { text = '󰐕 ' },
-            change       = { text = '󰧟 ' },
-            delete       = { text = '󰍴 ' },
-            topdelete    = { text = '󰅖 ' },
-            changedelete = { text = '󰏰 ' },
-            untracked    = { text = '󰍉 ' },
+            add          = { text = '' },
+            change       = { text = '' },
+            delete       = { text = '' },
+            topdelete    = { text = '' },
+            changedelete = { text = '' },
+            untracked    = { text = '' },
         },
 
         signs_staged_enable = true,
