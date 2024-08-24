afdc00d612b939d08f45da83885cdb1f72235588
AyimanyMooncat <mooncatayimany@proton.me>
2024-08-24 14:27:48 -0600

    Add to clangd extensions

--- lua/plugins/clangd_extensions.lua
+++ lua/plugins/clangd_extensions.lua
@@ -2,41 +2,74 @@ return {
     'p00f/clangd_extensions.nvim',
     name = 'Clangd Extensions',
 
-    ft   = { 'c', 'cpp' },
+
+    ft = { 'c', 'cpp' },
+    -- Move to AutoCmd
+    keys = {
+        {
+            '<leader>CS',
+            '<cmd>ClangdSwitchSourceHeader<CR>'
+        },
+        {
+            '<leader>Ch',
+            '<cmd>ClangdToggleInlayHints<CR>'
+        },
+        {
+            '<leader>CT',
+            '<cmd>ClangdAST<CR>'
+        },
+        {
+            '<leader>Cs',
+            '<cmd>ClangdSymbolInfo<CR>'
+        },
+        {
+            '<leader>Ct',
+            '<cmd>ClangdTypeHierarchy<CR>'
+        },
+        {
+            '<leader>Cm',
+            '<cmd>ClangdMemoryUsage<CR>'
+        },
+    },
+
 
     opts = {
         inlay_hints = {
-            parameter_hints_prefix = '󰁍 ',
-            other_hints_prefix = '󰁔 ',
+            parameter_hints_prefix = '  ',
+            other_hints_prefix = '  ',
         },
+
 
         ast = {
             role_icons = {
-                type = ' ',
-                declaration = ' ',
-                expression = ' ',
-                specifier = ' ',
-                statement = ' ',
+                type = '  ',
+                declaration = '  ',
+                expression = '  ',
+                specifier = '  ',
+                statement = '  ',
                 ['template argument'] = ' ',
             },
 
+
             kind_icons = {
-                Compound = ' ',
-                Recovery = ' ',
-                TranslationUnit = ' ',
-                PackExpansion = ' ',
-                TemplateTypeParm = ' ',
-                TemplateTemplateParm = ' ',
-                TemplateParamObject = ' ',
+                Compound = '  ',
+                Recovery = '  ',
+                TranslationUnit = '  ',
+                PackExpansion = '  ',
+                TemplateTypeParm = '  ',
+                TemplateTemplateParm = '  ',
+                TemplateParamObject = '  ',
             }
         },
+
 
         memory_usage = {
             border = 'rounded',
         },
 
+
         symbol_info = {
             border = 'rounded',
-        },
+        }
     }
 }
