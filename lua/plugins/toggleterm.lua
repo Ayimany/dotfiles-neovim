bf153ffd7249a29c804eef9cce2036b8e5bf72d2
AyimanyMooncat <mooncatayimany@proton.me>
2024-08-24 15:39:22 -0600

    Add toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+    'akinsho/toggleterm.nvim',
+    name = 'Toggleterm',
+
+
+    keys = {
+        {
+            '<leader><leader>',
+            '<cmd>ToggleTerm direction=float<CR>',
+            desc = 'Toggle Terminal'
+        }
+    },
+
+
+    config = true
+}
