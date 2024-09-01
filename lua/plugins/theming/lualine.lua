30fe0ddd4875e26d7b5496dcd1fdad02f7239215
AyimanyMooncat <mooncatayimany@proton.me>
2024-08-31 20:12:11 -0600

    Add lualine

---
+++ lua/plugins/theming/lualine.lua
@@ -0,0 +1,27 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    name = 'Theming::lualine',
+    main = 'lualine',
+
+    opts = {
+        options = {
+            icons_enabled = true,
+            theme = 'catppuccin',
+            component_separators = { left = '', right = ''},
+            section_separators   = { left = '', right = ''},
+        },
+
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff' },
+            lualine_c = { 'filename', 'diagnostics' },
+
+            lualine_x = { 'filesize', 'encoding', 'fileformat', 'filetype' },
+            lualine_y = { },
+            lualine_z = { 'location', 'progress', 'selectioncount', 'searchcount' }
+        }
+},
+
+    config = true
+}
+
