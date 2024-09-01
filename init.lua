56fc85ee8b2e22ba09f93e2f94b639b2f1be7fa5
AyimanyMooncat <mooncatayimany@proton.me>
2024-08-31 19:45:55 -0600

    Simplify Main `init.lua` file

--- init.lua
+++ init.lua
@@ -1,72 +1,44 @@
--- ooooo      ooo oooooooooooo   .oooooo. oooooo     oooo ooooo ooo        ooooo
--- `888b.     `8' `888'     `8  d8P'  `Y8b `888.     .8'  `888' `88.       .888'
---  8 `88b.    8   888         888      888 `888.   .8'    888   888b     d'888
---  8   `88b.  8   888oooo8    888      888  `888. .8'     888   8 Y88. .P  888
---  8     `88b.8   888    "    888      888   `888.8'      888   8  `888'   888
---  8       `888   888       o `88b    d88'    `888'       888   8    Y     888
--- o8o        `8  o888ooooood8  `Y8bood8P'      `8'       o888o o8o        o888o
 
--- |\/| _    _ ._ _  _ .__|_
--- |  |(_)\/(/_| | |(/_| ||_
-
+-- Efficient searching
 vim.opt.incsearch      = true
 vim.opt.ignorecase     = true
 vim.opt.smartcase      = true
+vim.opt.hlsearch       = true
 
---  _
--- | \o _._ | _.
--- |_/|_>|_)|(_|\/
---       |      /
-
-vim.opt.smoothscroll   = true
+-- Text visibility
 vim.opt.scrolloff      = 8
 vim.opt.sidescrolloff  = 16
 vim.opt.wrap           = false
-vim.opt.cmdheight      = 1
+
+-- Line numbers
 vim.opt.number         = true
 vim.opt.relativenumber = true
 
--- |_|o _ |_ |o _ |__|_o._  _
--- | ||(_|| |||(_|| ||_|| |(_|
---      _|      _|          _|
-
-vim.opt.background     = 'dark'
-vim.opt.hlsearch       = true
+-- Term colors
 vim.opt.termguicolors  = true
+
+-- Cursor hints
 vim.opt.cursorcolumn   = false
 vim.opt.cursorline     = true
 vim.opt.colorcolumn    = '80'
 
--- |\/|   |_|_o    o._  _| _
--- |  ||_|| |_|\/\/|| |(_|(_)\/\/
-
+-- Buffer splitting
 vim.opt.switchbuf      = 'split'
 
--- |\/| _     _ _
--- |  |(_)|_|_>(/_
-
--- disable mouse
+-- Disable mouse
 vim.opt.mouse          = ''
 
--- |\/| _     _ _
--- |  |(_)|_|_>(/_
-
+-- Selection mode
 vim.opt.selection      = 'inclusive'
 
---  _
--- |_ _|o_|_o._  _
--- |_(_|| |_|| |(_|
---               _|
-
+-- Generate undofile
 vim.opt.undofile       = true
 vim.opt.undodir        = os.getenv('XDG_CONFIG_HOME') .. '/nvim/undo'
+
+-- Custom pairs
 vim.opt.matchpairs     = '(:),{:},[:],<:>'
 
--- ___
---  | ._  _| _ .__|_o._  _
--- _|_| |(_|(/_| ||_|| |(_|
---                       _|
-
+-- Indenting
 vim.opt.tabstop        = 4
 vim.opt.shiftwidth     = 4
 vim.opt.softtabstop    = 4
@@ -78,18 +50,15 @@ vim.opt.smartindent    = true
 vim.opt.copyindent     = true
 vim.opt.preserveindent = true
 
---  _                  _
--- |_ _|_ _ ._._  _.| /  _ ._ _ ._ _  _.._  _| _
--- |_><|_(/_| | |(_|| \_(_)| | || | |(_|| |(_|_>
-
+-- Use fish shell
 vim.opt.shell          = '/usr/bin/fish'
 
--- |\/|o _ _
--- |  ||_>(_
-
+-- Enable signcolumn
 vim.opt.signcolumn     = 'yes'
+
+-- Leader key
 vim.g.mapleader        = ' '
 
+-- Extended functionality
 require('bootstrap')
 
-vim.cmd.colorscheme('catppuccin')
